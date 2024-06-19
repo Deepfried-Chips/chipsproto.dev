89009ed8f021aa63772d7f5aaf751011bdbc0d05
Deepfried-Chips <specificallydeepfriedchips@gmail.com>
2024-06-19 18:39:22 +0300

    ghhh

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -36,10 +36,6 @@ class OAuthController extends Controller
     {
         Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
         return redirect(route('blog'));
     }
 }
