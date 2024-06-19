a9364c30bcf3c63e985d0a4b5cb4a6480e26aa84
Deepfried-Chips <specificallydeepfriedchips@gmail.com>
2024-06-19 23:36:18 +0300

    heh

--- resources/js/Pages/Admin/Admin.tsx
+++ resources/js/Pages/Admin/Admin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {PageProps, Post} from "@/types";
 import AdminLayout from "@/Components/Admin/AdminLayout";
 import {Editor} from "@/Components/Admin/Blog/Editor";
@@ -7,6 +7,14 @@ import {Button} from "@mui/material";
 import {Link} from "@inertiajs/react";
 
 export default function Admin({auth, posts}: PageProps<{posts: Post[]}>) {
+    const [post, setPost] = useState<Post | null>(null)
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const post = posts.find(post => post.id === parseInt(e.currentTarget.name));
+        if (post) {
+            setPost(post);
+        }
+    }
 
     return (
         <AdminLayout user={auth?.user}>
@@ -14,15 +22,17 @@ export default function Admin({auth, posts}: PageProps<{posts: Post[]}>) {
                 <div className="w-1/6 h-full bg-orange-500 rounded-lg mr-2 flex flex-col justify-between text-black">
                     <div className="overflow-auto flex justify-center items-center h-screen text-2xl font-bold">
                         {posts.map((post) => (
-                            <Link href={route('admin.edit', post.id)}>{post.title}</Link>
+                            <Button key={post.id} name={post.id} onClick={handleClick}>{post.title}</Button>
                         ))}
                     </div>
-                    <div className="mb-4 flex justify-center">
+                    <div className="mb-4 flex flex-row justify-center">
+                        <Button href={route('admin')}>EDIT</Button>
                         <Button href={route('admin')}>NEW</Button>
                     </div>
                 </div>
-                <div className="w-full h-full bg-black rounded-lg">
-
+                <div className="w-full h-full rounded-lg flex flex-col items-center">
+                    <h1 className="text-3xl font-bold">Click on a post name to get started UwU</h1>
+                    <p>Preview will be here once I write the code for that, right now clicking on a post sends you to the edit page, oopsies :3</p>
                 </div>
             </div>
         </AdminLayout>
